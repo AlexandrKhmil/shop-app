53ae4b0259725139e07444995a6d07f053179067
AlexandrKhmil <oleksandr.khmil2@gmail.com>
2020-04-23 22:46:37 +0300

    installed react

---
+++ client/src/containers/App/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from "./styles.module.css";
+
+function App() {
+  return (
+    <div className={styles.a}>
+      Hello World from React2!!!
+      <p>
+        Add some Tags!
+      </p>
+      <p>Another Tag</p>
+    </div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -7,16 +8,16 @@ const db = pgp('postgres://postgres:root@localhost:5432/flower-shop');
 
 app.get('/2', (req, res) => {
   db.any(`SELECT p.*, 
-	AVG(rate_value) p_rate, 
-	COUNT(rate_value) AS p_votes_count,
-	COUNT(rev_id) AS p_reviews_count,
-	array(SELECT ptag_id
-		FROM product_has_tag AS pht
-		WHERE pht.p_id = p.p_id) AS p_tag
+  AVG(rate_value) p_rate, 
+  COUNT(rate_value) AS p_votes_count,
+  COUNT(rev_id) AS p_reviews_count,
+  array(SELECT ptag_id
+    FROM product_has_tag AS pht
+    WHERE pht.p_id = p.p_id) AS p_tag
 FROM product AS p
-	LEFT JOIN rate ON p.p_id = rate.p_id
-	LEFT JOIN review AS rev ON rev.rate_id = rate.rate_id
-	LEFT JOIN product_has_tag AS pht ON p.p_id = pht.p_id
+  LEFT JOIN rate ON p.p_id = rate.p_id
+  LEFT JOIN review AS rev ON rev.rate_id = rate.rate_id
+  LEFT JOIN product_has_tag AS pht ON p.p_id = pht.p_id
 WHERE pst_id = 1
 GROUP BY p.p_id
 ORDER BY p.p_id LIMIT 5 OFFSET 0;`, 123)
@@ -30,23 +31,23 @@ ORDER BY p.p_id LIMIT 5 OFFSET 0;`, 123)
 
 app.get('/1', (req, res) => {
   db.any(`SELECT p.*,
-	AVG(rate_value) p_rate, 
-	COUNT(rate_value) AS p_votes_count,
-	COUNT(rev_id) AS p_reviews_count,
-	ARRAY(SELECT ptag_id
-		FROM product_has_tag AS pht
-		WHERE pht.p_id = p.p_id) AS p_tag,
-	(SELECT json_agg(t) 
-	 FROM (SELECT review.*, COUNT(review_like.rev_id) AS rev_likes
-		   FROM review
-		   	   INNER JOIN rate ON rate.rate_id = review.rate_id
-	 	   	   LEFT JOIN review_like ON review.rev_id = review_like.rev_id
- 		   WHERE rate.p_id = p.p_id
-	 	   GROUP BY review.rev_id) AS t) AS p_review
+  AVG(rate_value) p_rate, 
+  COUNT(rate_value) AS p_votes_count,
+  COUNT(rev_id) AS p_reviews_count,
+  ARRAY(SELECT ptag_id
+    FROM product_has_tag AS pht
+    WHERE pht.p_id = p.p_id) AS p_tag,
+  (SELECT json_agg(t) 
+   FROM (SELECT review.*, COUNT(review_like.rev_id) AS rev_likes
+       FROM review
+            INNER JOIN rate ON rate.rate_id = review.rate_id
+             LEFT JOIN review_like ON review.rev_id = review_like.rev_id
+        WHERE rate.p_id = p.p_id
+        GROUP BY review.rev_id) AS t) AS p_review
 FROM product AS p
-	LEFT JOIN rate ON p.p_id = rate.p_id
-	LEFT JOIN review AS rev ON rev.rate_id = rate.rate_id
-	LEFT JOIN product_has_tag AS pht ON p.p_id = pht.p_id
+  LEFT JOIN rate ON p.p_id = rate.p_id
+  LEFT JOIN review AS rev ON rev.rate_id = rate.rate_id
+  LEFT JOIN product_has_tag AS pht ON p.p_id = pht.p_id
 WHERE pst_id = 1
 GROUP BY p.p_id;`)
     .then(function (data) {
