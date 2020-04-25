654e747849196409c249ba43eb137b16df2d4ce4
AlexandrKhmil <oleksandr.khmil2@gmail.com>
2020-04-25 12:35:27 +0300

    user routes

--- docs/shop-sql-requests/user-register.pgsql
+++ docs/shop-sql-requests/user-register.pgsql
@@ -1,5 +1,23 @@
 -- Register user
 
+--
+DECLARE
+  v_isExist boolean;
+
+BEGIN
+  v_isExist = (SELECT COUNT(1) > 0
+               FROM "user"
+               WHERE u_email = arg_user_email);
+
+  IF (v_isExist) THEN
+  	RAISE 'USER EXIXSTS';
+  END IF;
+
+  INSERT INTO "user" (u_email, u_password)
+    VALUES (arg_user_email, arg_user_password);
+  RETURN currval('user_u_id_seq');
+END;
+
 -- If returns 0
 SELECT COUNT(1)
 FROM "user"
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,51 +1,117 @@
 const { Router } = require('express');
 const router = Router();
+const config = require('config');
+const jwtSecret = config.get('jwtSecret');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, body, validationResult } = require('express-validator')
 const db = require('../connection');
 
 // GET `api/user`
-router.get('/', async (req, res) => {
-  try {
-    const token = req.headers.user_token;
-    if (token) {
-      const decodeToken = token; // decode
+router.get('/', [
+    check('token', 'Wrong token').exists()
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(500).json({ errors: errors.array() });
+      }
+
+      const decodedToken = jwt.decode(req.headers.token, jwtSecret);
       const query = `
         SELECT
-          u_email AS email,
-          u_password AS password
+          u_email AS email
         FROM "user"
         WHERE u_id = $1;
       `;
-      db.any(query, decodeToken)
-        .then((data) => {
-          return data[0]
-            ? res.status(200).json({ ...data })
-            : res.status(500).json({ message: 'Нет такого пользователя'});
-        })
-        .catch((error) => {
-          console.log('error');
-        });  
-    } else {
-      return res.status(200).json({ message : 'No token'});
-    } 
-  } catch (e) {
-    return res.status(500).json({ message: 'Ошибка' });
-  }
-});
+      const result = await db.one(query, decodedToken.id)
+        .then((data) => data)
+        .catch((error) => ({ error }));
+      if (result.error) {
+        return res.status(500).json({ message: 'No such user' });
+      }
+      return res.status(200).json(result);
+    } catch (e) {
+      return res.status(500).json({ message: e });
+    }
+  });
 
 // POST `api/user/login`
-router.post('/login', async(req, res) => {
-  try {
+router.post('/login', [
+    body('email', 'Wrong email').normalizeEmail().isEmail(),
+    body('password', 'Wrong password').isLength({ min: 5 })
+  ],
+  async(req, res) => {
+    try { 
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(500).json({ errors: errors.array() });
+      }
 
-  } catch(e) {
-    return res.status(500).jsoin({ message: 'Ошибка '});
-  }
-});
+      const { email, password } = req.body;
+      const query = `
+        SELECT
+          u_id AS id,
+          u_password AS password
+        FROM "user"
+        WHERE u_email = $1;
+      `;
+      const result = await db.one(query, email)
+        .then((data) => data)
+        .catch((error) => ({ error })); 
+
+      if (result.error) {
+        return res.status(500).json(result);
+      } 
+      const isMatch = await bcrypt.compare(password, result.password)
+      if (!isMatch) {
+        return res.status(500).json({ message: 'Wrong password' });
+      }
+
+      const token = jwt.sign(
+        { id: result.id },
+        jwtSecret,
+        { expiresIn: '1h' }
+      );
+      return res.status(200).json({ token, email });
+    } catch(e) { 
+      return res.status(500).json({ message: e });
+    }
+  });
 
 
 // POST `api/user/register`
-router.post('/register', async(req, res) => {
+router.post('/register', [
+    check('email', 'Wrong email').normalizeEmail().isEmail(),
+    check('password', 'Wrong password').isLength({ min: 5 })
+  ],
+  async(req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(500).json({ errors: errors.array() });
+      }
 
-});
+      const { email, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const result = await db.func('user_register', [email, hashedPassword])
+        .then(data => data[0].user_register)
+        .catch(error => ({ error }));
+      
+      if (result.error) {
+        return res.status(500).json({ message: 'User exists' });
+      }
+      const token = jwt.sign(
+        { id: result },
+        jwtSecret,
+        { expiresIn: '1h' }
+      );
+      return res.status(200).json({ token, email });
+    } catch(e) {
+      return res.status(500).json({ message: e });
+    }
+  });
 
 
 // POST `api/user/change-password`
